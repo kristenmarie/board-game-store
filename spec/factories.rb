552f4e797112139300a07256917bac0589ac0970
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-01 12:39:23 -0800

    Add integration tests for product editing pages.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,17 @@
 FactoryBot.define do
   factory :product do
-    name 'Sherlock Holes Consulting Detective'
+    title 'Sherlock Holes Consulting Detective'
     price 34
     description 'Ten Sherlock Holmes adventures in which you are the hero! You are a member of the Baker Street Irregulars working to solve mysteries before the Master! You are either investigating the mysteries as an individual, or trying to win against up to 8 other investigators.'
     players '1-8'
     playtime '60-120 minutes'
-    publish 'Asmodee Editions'
+    publisher 'Asmodee Editions'
+  end
+
+  factory :user do
+    username 'admin'
+    email 'admin@admin.com'
+    password '123456'
+    admin true
   end
 end
---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'The product editing process' do
+  it 'will allow admin to edit product' do
+    product = FactoryBot.create(:product)
+    visit edit_product_path(product)
+    fill_in 'Title', :with => 'Dead of Winter'
+    click_on 'Update Product'
+    expect(page). to have_content 'has been updated'
+  end
+
+  it 'will display errors if field left blank' do
+    product = FactoryBot.create(:product)
+    visit edit_product_path(product)
+    fill_in 'Title', :with => ' '
+    click_on 'Update Product'
+    expect(page). to have_content "Title can't be blank"
+  end
+
+end
