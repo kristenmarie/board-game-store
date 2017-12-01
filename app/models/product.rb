d0e28deaee93514aeb35819ace6bdbaec9439c8b
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-01 13:13:09 -0800

    Add test for product recently added scope.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   validates :title, :price, :description, :players, :playtime, :publisher, :presence => true
 
-  scope :recently_added, -> { order(created_at: :desc).limit(10) }
+  scope :recently_added, -> { order(created_at: :desc).limit(5) }
   has_many :reviews
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many :reviews }
+
+  describe '.recently_added' do
+    it 'should return 5 most recently added products' do
+      g1 = FactoryBot.create(:product)
+      g2 = FactoryBot.create(:product)
+      g3 = FactoryBot.create(:product)
+      g4 = FactoryBot.create(:product)
+      g5 = FactoryBot.create(:product)
+      g6 = FactoryBot.create(:product)
+      expect(Product.recently_added).to eq([g6, g5, g4, g3, g2])
+    end
+  end
 end
