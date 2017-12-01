009cf54e5493e31b5e13feaddc13237b3fce19e9
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-01 13:07:50 -0800

    Add tests for relationship between products and reviews.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   validates :title, :price, :description, :players, :playtime, :publisher, :presence => true
 
   scope :recently_added, -> { order(created_at: :desc).limit(10) }
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
   validates :rating, :heading, :content, :name, :presence => true
+  belongs_to :product
 end
