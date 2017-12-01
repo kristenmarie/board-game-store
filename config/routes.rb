8c27651a1311db47d061f8afa23b7cd22cfe5d24
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-01 10:53:20 -0800

    Add views for product crud.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root :to => 'products#index'
 
   resources :products do
+    resources :reviews
+  end
+
+  resources :users do
     resources :reviews
   end
 end
