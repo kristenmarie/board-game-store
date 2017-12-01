444d1ab02ff464504e3d381648db3f739c5dbdd9
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-01 12:46:31 -0800

    Add extra integration test to edit product pages.

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -1,21 +1,28 @@
 require 'rails_helper'
 
 describe 'The product editing process' do
+  before do
+    @product = FactoryBot.create(:product)
+  end
 
   it 'will allow admin to edit product' do
-    product = FactoryBot.create(:product)
-    visit edit_product_path(product)
+    visit edit_product_path(@product)
     fill_in 'Title', :with => 'Dead of Winter'
     click_on 'Update Product'
     expect(page). to have_content 'has been updated'
   end
 
   it 'will display errors if field left blank' do
-    product = FactoryBot.create(:product)
-    visit edit_product_path(product)
+    visit edit_product_path(@product)
     fill_in 'Title', :with => ' '
     click_on 'Update Product'
     expect(page). to have_content "Title can't be blank"
+  end
+
+  it 'will only allow admin to edit product' do
+    user = FactoryBot.create(:user)
+    visit product_path(@product)
+    expect(page).to have_no_content 'Edit'
   end
 
 end
