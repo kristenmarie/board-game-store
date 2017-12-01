fa6272788b74fcd8da062186e5254b6b9edc03ce
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-01 09:43:42 -0800

    Add crud to products controller and create index view.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "#{@product.title} has been added to the inventory!"
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "#{@product.title} has been updated!"
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:alert] = "Product has been removed from inventory."
+    redirect_to products_path
+  end
+
+
+
+private
+  def product_params
+    params.require(:product).permit(:title, :price, :description, :players, :playtime, :publisher)
+  end
+end
