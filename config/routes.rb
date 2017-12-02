9f0d0664f26c48ba949f56fb907c37ad53453241
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-01 16:01:12 -0800

    Add basic styles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root :to => 'products#index'
+  root :to => 'products#home'
 
   resources :products do
     resources :reviews
